8faf0180520fca5b13b6b676cac3593e41cefd37
Sidd-Rawlani <sidhu.rawlani@gmail.com>
2023-10-13 18:39:34 +0530

    first commit

--- Detector.py
+++ Detector.py
@@ -1,48 +1,71 @@
 import cv2
-from time import sleep
-from PIL import Image 
-
-def main_app(name):
-        
-        face_cascade = cv2.CascadeClassifier('./data/haarcascade_frontalface_default.xml')
-        recognizer = cv2.face.LBPHFaceRecognizer_create()
-        recognizer.read(f"./data/classifiers/{name}_classifier.xml")
-        cap = cv2.VideoCapture(0)
-        pred = 0
-        while True:
-            ret, frame = cap.read()
-            #default_img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            faces = face_cascade.detectMultiScale(gray,1.3,5)
-
-            for (x,y,w,h) in faces:
+from PIL import Image
+from geopy.geocoders import Nominatim
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
 
 
-                roi_gray = gray[y:y+h,x:x+w]
+def send_email(to_email):
+    from_email = "Snipe729@gmail.com"
+    password = "Snipe.sni"  # Be cautious when storing your password
+    subject = "Missing Person Detected"
+    message = "A missing person has been detected at the following location: "
 
-                id,confidence = recognizer.predict(roi_gray)
-                confidence = 100 - int(confidence)
-                pred = 0
-                if confidence > 50:
-                    #if u want to print confidence level
-                            #confidence = 100 - int(confidence)
-                            pred += +1
-                            text = name.upper()
-                            font = cv2.FONT_HERSHEY_PLAIN
-                            frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                            frame = cv2.putText(frame, text, (x, y-4), font, 1, (0, 255, 0), 1, cv2.LINE_AA)
+    msg = MIMEMultipart()
+    msg["From"] = from_email
+    msg["To"] = to_email
+    msg["Subject"] = subject
+    msg.attach(MIMEText(message, "plain"))
 
-                else:   
-                            pred += -1
-                            text = "UnknownFace"
-                            font = cv2.FONT_HERSHEY_PLAIN
-                            frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-                            frame = cv2.putText(frame, text, (x, y-4), font, 1, (0, 0,255), 1, cv2.LINE_AA)
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(from_email, password)
+    text = msg.as_string()
+    server.sendmail(from_email, to_email, text)
+    server.quit()
 
-            cv2.imshow("image", frame)
+def main_app(name, latitude, longitude, to_email):
+    
+    geolocator = Nominatim(user_agent="missing_person_app")
 
+    face_cascade = cv2.CascadeClassifier('./data/haarcascade_frontalface_default.xml')
+    recognizer = cv2.face_LBPHFaceRecognizer.create()
+    recognizer.read(f"./data/classifiers/{name}_classifier.xml")
+    cap = cv2.VideoCapture(0)
+    pred = 0
+    
+    while True:
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-            if cv2.waitKey(20) & 0xFF == ord('q'):
+        for (x, y, w, h) in faces:
+            roi_gray = gray[y:y+h, x:x+w]
+            id, confidence = recognizer.predict(roi_gray)
+            confidence = 100 - int(confidence)
+            pred = 0
+
+            if confidence > 30:
+                pred += 1
+                text = name.upper()
+                font = cv2.FONT_HERSHEY_PLAIN
+                frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                frame = cv2.putText(frame, text, (x, y - 4), font, 1, (0, 255, 0), 1, cv2.LINE_AA)
+                location = geolocator.reverse((latitude, longitude), exactly_one=True)
+                if location:
+                    frame = cv2.putText(frame, location.address, (x, y -18), font, 1, (0, 255, 0), 1, cv2.LINE_AA)
+                
+            else:
+                pred += -1
+                text = "UnknownFace"
+                font = cv2.FONT_HERSHEY_PLAIN
+                frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                frame = cv2.putText(frame, text, (x, y - 4), font, 1, (0, 0, 255), 1, cv2.LINE_AA)
+
+        cv2.imshow("image", frame)
+
+        if cv2.waitKey(20) & 0xFF == ord('q'):
                 print(pred)
                 if pred > 0 : 
                     dim =(124,124)
@@ -63,12 +86,12 @@ def main_app(name):
                     # save the image  
                     Image1copy.save("end.png") 
                     frame = cv2.imread("end.png", 1)
-
+                    send_email(to_email)
                     cv2.imshow("Result",frame)
                     cv2.waitKey(5000)
+                    
                 break
-
-
-        cap.release()
-        cv2.destroyAllWindows()
-        
+    
+       
+    cap.release()
+    cv2.destroyAllWindows()
--- app-gui.py
+++ app-gui.py
@@ -1,3 +1,4 @@
+from geopy.geocoders import Nominatim 
 from Detector import main_app
 from create_classifier import train_classifer
 from create_dataset import start_capture
@@ -20,26 +21,39 @@ class MainUI(tk.Tk):
                 names.add(i)
         self.title_font = tkfont.Font(family='Helvetica', size=16, weight="bold")
         self.title("Missing Person Detector")
-        self.resizable(False, False)
-        self.geometry("500x250")
+        self.resizable(True, True)
+        self.geometry("500x400")
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
         self.active_name = None
+        self.geolocator = Nominatim(user_agent="missing_person_app")  # Initialize Nominatim geocoder
         container = tk.Frame(self)
         container.grid(sticky="nsew")
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
         self.frames = {}
-        for F in (StartPage, PageOne, PageTwo, PageThree, PageFour):
+        self.frames["Login"] = Login(parent=container, controller=self)
+
+        for F in (Login, StartPage, PageOne, PageTwo, PageThree, PageFour):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
             self.frames[page_name] = frame
             frame.grid(row=0, column=0, sticky="nsew")
-        self.show_frame("StartPage")
+        # self.show_frame("StartPage")
+        self.show_frame("Login")
+
 
     def show_frame(self, page_name):
             frame = self.frames[page_name]
             frame.tkraise()
-
+    
+    def login(self, username, password):
+    
+        if username == "Sidd" and password == "Sidd":
+            self.show_frame("StartPage")  # Allow access to the main application
+            self.logged_in = True
+        else:
+            messagebox.showerror("Login Failed", "Invalid username or password")
+        
     def on_closing(self):
 
         if messagebox.askokcancel("Exit", "Are you sure?"):
@@ -48,6 +62,29 @@ class MainUI(tk.Tk):
             for i in names:
                     f.write(i+" ")
             self.destroy()
+
+class Login(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        self.username_label = tk.Label(self, text="Username", font='Helvetica 12 bold')
+        self.username_entry = tk.Entry(self, font='Helvetica 11')
+        self.password_label = tk.Label(self, text="Password", font='Helvetica 12 bold')
+        self.password_entry = tk.Entry(self, show="*", font='Helvetica 11')
+
+        self.username_label.grid(row=0, column=0, pady=10, padx=5)
+        self.username_entry.grid(row=0, column=1, pady=10, padx=10)
+        self.password_label.grid(row=1, column=0, pady=10, padx=5)
+        self.password_entry.grid(row=1, column=1, pady=10, padx=10)
+
+        self.login_button = tk.Button(self, text="Login", fg="#ffffff", bg="#000000", command=self.login)
+        self.login_button.grid(row=2, column=0, columnspan=2, ipadx=5, ipady=4, pady=20)
+
+    def login(self):
+        username = self.username_entry.get()
+        password = self.password_entry.get()
+        self.controller.login(username, password)
+        
 
 
 class StartPage(tk.Frame):
@@ -82,13 +119,36 @@ class PageOne(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
+
         tk.Label(self, text="Enter the Name", fg="#000000", font='Helvetica 12 bold').grid(row=0, column=0, pady=10, padx=5)
         self.user_name = tk.Entry(self, borderwidth=3, bg="lightgrey", font='Helvetica 11')
         self.user_name.grid(row=0, column=1, pady=10, padx=10)
+
+        tk.Label(self, text="Enter Email", fg="#000000", font='Helvetica 12 bold').grid(row=1, column=0, pady=10, padx=5)
+        self.user_email = tk.Entry(self, borderwidth=3, bg="lightgrey", font='Helvetica 11')
+        self.user_email.grid(row=1, column=1, pady=10, padx=10)
+
+        tk.Label(self, text="Enter Phone Number", fg="#000000", font='Helvetica 12 bold').grid(row=2, column=0, pady=10, padx=5)
+        self.user_phone = tk.Entry(self, borderwidth=3, bg="lightgrey", font='Helvetica 11')
+        self.user_phone.grid(row=2, column=1, pady=10, padx=10)
+
+        tk.Label(self, text="Enter Last Location", fg="#000000", font='Helvetica 12 bold').grid(row=3, column=0, pady=10, padx=5)
+        self.user_location = tk.Entry(self, borderwidth=3, bg="lightgrey", font='Helvetica 11')
+        self.user_location.grid(row=3, column=1, pady=10, padx=10)
+
+        tk.Label(self, text="Enter Height (cm)", fg="#000000", font='Helvetica 12 bold').grid(row=4, column=0, pady=10, padx=5)
+        self.user_height = tk.Entry(self, borderwidth=3, bg="lightgrey", font='Helvetica 11')
+        self.user_height.grid(row=4, column=1, pady=10, padx=10)
+
+        tk.Label(self, text="Enter Weight (kg)", fg="#000000", font='Helvetica 12 bold').grid(row=5, column=0, pady=10, padx=5)
+        self.user_weight = tk.Entry(self, borderwidth=3, bg="lightgrey", font='Helvetica 11')
+        self.user_weight.grid(row=5, column=1, pady=10, padx=10)
+
         self.buttoncanc = tk.Button(self, text="Cancel", bg="#ffffff", fg="#000000", command=lambda: controller.show_frame("StartPage"))
         self.buttonext = tk.Button(self, text="Next", fg="#ffffff", bg="#000000", command=self.start_training)
-        self.buttoncanc.grid(row=1, column=0, pady=10, ipadx=5, ipady=4)
-        self.buttonext.grid(row=1, column=1, pady=10, ipadx=5, ipady=4)
+
+        self.buttoncanc.grid(row=6, column=0, pady=10, ipadx=5, ipady=4)
+        self.buttonext.grid(row=6, column=1, pady=10, ipadx=5, ipady=4)
     def start_training(self):
         global names
         if self.user_name.get() == "None":
@@ -100,12 +160,21 @@ class PageOne(tk.Frame):
         elif len(self.user_name.get()) == 0:
             messagebox.showerror("Error", "Name cannot be empty!")
             return
+
+        # You can retrieve the user information from the fields like this:
         name = self.user_name.get()
+        to_email = self.user_email.get()
+        phone = self.user_phone.get()
+        location = self.user_location.get()
+        height = self.user_height.get()
+        weight = self.user_weight.get()
+
+        # Now, you can save this user information or use it as needed.
         names.add(name)
         self.controller.active_name = name
         self.controller.frames["PageTwo"].refresh_names()
         self.controller.show_frame("PageThree")
-
+        self.controller.frames("PageFour").to_email= to_email
 
 class PageTwo(tk.Frame):
 
@@ -166,6 +235,26 @@ class PageThree(tk.Frame):
         self.controller.show_frame("PageFour")
 
 
+# class PageFour(tk.Frame):
+
+#     def __init__(self, parent, controller):
+#         tk.Frame.__init__(self, parent)
+#         self.controller = controller
+
+#         label = tk.Label(self, text="Face Recognition", font='Helvetica 16 bold')
+#         label.grid(row=0,column=0, sticky="ew")
+#         button1 = tk.Button(self, text="Face Recognition", command=self.openwebcam, fg="#ffffff", bg="#000000")
+#         #button2 = tk.Button(self, text="Emotion Detection", command=self.emot, fg="#ffffff", bg="#263942")
+#         #button3 = tk.Button(self, text="Gender and Age Prediction", command=self.gender_age_pred, fg="#ffffff", bg="#263942")
+#         button4 = tk.Button(self, text="Go to Home Page", command=lambda: self.controller.show_frame("StartPage"), bg="#ffffff", fg="#000000")
+#         button1.grid(row=1,column=0, sticky="ew", ipadx=5, ipady=4, padx=10, pady=10)
+#         #button2.grid(row=1,column=1, sticky="ew", ipadx=5, ipady=4, padx=10, pady=10)
+#         #button3.grid(row=2,column=0, sticky="ew", ipadx=5, ipady=4, padx=10, pady=10)
+#         button4.grid(row=1,column=1, sticky="ew", ipadx=5, ipady=4, padx=10, pady=10)
+
+#     def openwebcam(self):
+#         main_app(self.controller.active_name)
+
 class PageFour(tk.Frame):
 
     def __init__(self, parent, controller):
@@ -173,20 +262,42 @@ class PageFour(tk.Frame):
         self.controller = controller
 
         label = tk.Label(self, text="Face Recognition", font='Helvetica 16 bold')
-        label.grid(row=0,column=0, sticky="ew")
+        label.grid(row=0, column=0, sticky="ew")
         button1 = tk.Button(self, text="Face Recognition", command=self.openwebcam, fg="#ffffff", bg="#000000")
-        #button2 = tk.Button(self, text="Emotion Detection", command=self.emot, fg="#ffffff", bg="#263942")
-        #button3 = tk.Button(self, text="Gender and Age Prediction", command=self.gender_age_pred, fg="#ffffff", bg="#263942")
+        button2 = tk.Button(self, text="Geocoding", command=self.geocode_address, fg="#ffffff", bg="#000000")
+        button3 = tk.Button(self, text="Reverse Geocoding", command=self.reverse_geocode, fg="#ffffff", bg="#000000")
         button4 = tk.Button(self, text="Go to Home Page", command=lambda: self.controller.show_frame("StartPage"), bg="#ffffff", fg="#000000")
-        button1.grid(row=1,column=0, sticky="ew", ipadx=5, ipady=4, padx=10, pady=10)
-        #button2.grid(row=1,column=1, sticky="ew", ipadx=5, ipady=4, padx=10, pady=10)
-        #button3.grid(row=2,column=0, sticky="ew", ipadx=5, ipady=4, padx=10, pady=10)
-        button4.grid(row=1,column=1, sticky="ew", ipadx=5, ipady=4, padx=10, pady=10)
+        button1.grid(row=1, column=0, sticky="ew", ipadx=5, ipady=4, padx=10, pady=10)
+        button2.grid(row=1, column=1, sticky="ew", ipadx=5, ipady=4, padx=10, pady=10)
+        button3.grid(row=2, column=0, sticky="ew", ipadx=5, ipady=4, padx=10, pady=10)
+        button4.grid(row=2, column=1, sticky="ew", ipadx=5, ipady=4, padx=10, pady=10)
 
-    def openwebcam(self):
-        main_app(self.controller.active_name)
+    def openwebcam(self,to_email=None):
+        latitude = 19.0760  
+        longitude = 72.8777 
+        self.to_email = to_email
+        main_app(self.controller.active_name, latitude, longitude, to_email)
+        
 
-    
+    def geocode_address(self):
+        # Example of geocoding an address
+        address = "Mumbai"
+        location = self.controller.geolocator.geocode(address)
+        if location:
+            messagebox.showinfo("Geocoding Result", f"Location: {location.latitude}, {location.longitude}")
+        else:
+            messagebox.showerror("Geocoding Error", "Address not found.")
+
+    def reverse_geocode(self):
+        # Example of reverse geocoding coordinates
+        latitude = 19.0760
+        longitude = 72.8777
+        location = self.controller.geolocator.reverse((latitude, longitude))
+        if location:
+            messagebox.showinfo("Reverse Geocoding Result", f"Address: {location.address}")
+        else:
+            messagebox.showerror("Reverse Geocoding Error", "Coordinates not found.")
+
 
 
 
